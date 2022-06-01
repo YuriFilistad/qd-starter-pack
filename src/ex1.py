036dd7c36919a505d5b5d8c6c9ca6bbfcfe23175
YuriFilistad <101092004+YuriFilistad@users.noreply.github.com>
2022-06-01 18:31:56 +0200

    improve code

---
+++ src/ex1.py
@@ -0,0 +1,20 @@
+def get_day(day: int) -> str:
+    match day:
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+        case 7:
+            return "Sunday"
+        case _:
+            return "Invalid input"
+
+#print(get_day(int(input("Enter day number(1-7):"))))
